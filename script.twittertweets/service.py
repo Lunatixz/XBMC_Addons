913681f66ba99c0bb81f140d62afed55f34006e8
Lunatixz <Kgraer@gmail.com>
2017-08-18 16:25:04 -0400

    Unsplash v.1.0.0, TwitterTweets v.1.0.1a

--- script.twittertweets/service.py
+++ script.twittertweets/service.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Twitter Tweets.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, gui, datetime, random, urllib2, re
+import os, gui, time, datetime, random, urllib2, re
 import xbmc, xbmcaddon, xbmcgui, traceback
 
 from bs4 import BeautifulSoup
@@ -57,35 +57,46 @@ def setProperty(str1, str2):
 
 def clearProperty(str):
     xbmcgui.Window(10000).clearProperty((str))
-   
+        
 class Service():
     def __init__(self):
         log('__init__')
-        self.myService = xbmc.Monitor()
+        self.myMonitor = xbmc.Monitor()
         self.start()
         
         
     def start(self):
-        while not self.myService.abortRequested():
-            self.userList = []
-            for i in range(1,51):
-                self.userList.append((REAL_SETTINGS.getSetting('FEED%d'%i)).replace('@',''))
-            self.userList = filter(None, self.userList)
-            log('userList = ' + str(self.userList))
-
-            WAIT_TIME = [300,600,900,1800][int(REAL_SETTINGS.getSetting('Wait_Time'))]
-            IGNORE    = REAL_SETTINGS.getSetting('Not_While_Playing') == 'true'
-            if xbmc.Player().isPlayingVideo() == True and IGNORE == True:
-                self.myService.waitForAbort(WAIT_TIME)
+        log('start')
+        while not self.myMonitor.abortRequested():
+            self.checkSettings()
+            # Dont run while playing.
+            if xbmc.Player().isPlayingVideo() == True and self.ignore == True:
+                self.myMonitor.waitForAbort(self.wait)
+                continue
+                
+            # Avoid running while settings open.
+            if xbmcgui.getCurrentWindowDialogId() == 10140:
+                self.myMonitor.waitForAbort(1)
                 continue
                 
             for user in self.userList:
                 self.chkFEED(user)
                 
-            if self.myService.waitForAbort(WAIT_TIME) == True:
+            if self.myMonitor.waitForAbort(self.wait) == True:
                 break
 
-                
+            
+    def checkSettings(self):
+        xbmc.sleep(1000)
+        self.wait   = [300,600,900,1800][int(REAL_SETTINGS.getSetting('Wait_Time'))]
+        self.ignore = REAL_SETTINGS.getSetting('Not_While_Playing') == 'true'
+        self.userList = []
+        for i in range(1,51):
+            self.userList.append((REAL_SETTINGS.getSetting('FEED%d'%i)).replace('@',''))
+        self.userList = filter(None, self.userList)
+        log('checkSettings, userList = ' + str(self.userList))  
+    
+    
     def testString(self):
         ''' 
         gen. 140char mock sentence for skin test
@@ -100,8 +111,20 @@ class Service():
         return re.sub(r"http\S+", "", string)
         
         
+    def correctTime(self, tweetTime):
+        log('correctTime, IN tweetTime = '+tweetTime)
+        tweetTime = datetime.datetime.strptime(tweetTime, '%I:%M %p - %d %b %Y')
+        is_dst = time.daylight and time.localtime().tm_isdst > 0
+        utc_offset = + (time.altzone if is_dst else time.timezone)
+        td_local = datetime.timedelta(seconds=utc_offset-3600)
+        t = tweetTime + td_local
+        tweetTime = t.strftime('%I:%M %p - %d %b %Y').lstrip('0')
+        log('correctTime, OUT tweetTime = '+tweetTime)
+        return tweetTime
+        
+        
     def chkFEED(self, user):
-        log('chkFEED, user='+user)
+        log('chkFEED, user = '+user)
         try:
             soup       = BeautifulSoup(urllib2.urlopen(BASE_URL+user).read(), "html.parser")
             twitterPic = soup('img' , {'class': 'ProfileAvatar-image'})[0].attrs['src']
@@ -120,7 +143,7 @@ class Service():
                 if user.lower() in item.attrs['href'].lower():
                     break
                     
-            tweetTime  = tweetTimes[idx]["title"].encode("utf-8")
+            tweetTime  = self.correctTime(tweetTimes[idx]["title"].encode("utf-8"))
             tweetMsg   = self.cleanString(tweetMsgs[idx].get_text().encode("utf-8"))
             tweetStats = [stat.get_text().encode("utf-8") for stat in tweetStats]
             tweetStats = [tweetStats[x:x+3] for x in xrange(0, len(tweetStats), 3)]
